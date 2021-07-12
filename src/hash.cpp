09b12c15b98e69af54cd5a324e8ee07561322481
spoturno <spoturno16@gmail.com>
2021-07-12 19:16:39 -0300

    sha256_transform implemented

--- src/hash.cpp
+++ src/hash.cpp
@@ -37,3 +37,42 @@ static const WORD K[64] =
 };
 
 
+void sha256_transform(SHA256_CTX *ctx, const BYTE data[]){
+    WORD a, b, c, d, e, f, g, h, i, j, t1, t2, m[64]; // i & j count var
+
+    for(i = 0, j = 0; i < 16; i++, j+=4)
+        m[i] = (data[j] << 24) | (data[j+1] << 16) | (data[j+2] << 8) | (data[j+3]);
+    for( ; i < 64; i++)
+        m[i] = SIG1(m[i - 2]) + m[i - 7] + SIG0(m[i - 15]) + m[i - 16];
+
+    a = ctx->state[0];
+    b = ctx->state[1];
+    c = ctx->state[2];
+    d = ctx->state[3];
+    e = ctx->state[4];
+    f = ctx->state[5];
+    g = ctx->state[6];
+    h = ctx->state[7];
+
+    for(i=0; i<64; i++){
+        t1 = h + EP1(e) + CH(e, f, g) + K[i] + m[i];
+        t2 = EP0(a) + MAJ(a, b, c);
+        h = g;
+        g = f;
+        f = e;
+        e = d + t1;
+        d = c;
+        c = b;
+        b = a;
+        a = t1 + t2;
+    }
+
+	ctx->state[0] += a;
+	ctx->state[1] += b;
+	ctx->state[2] += c;
+	ctx->state[3] += d;
+	ctx->state[4] += e;
+	ctx->state[5] += f;
+	ctx->state[6] += g;
+	ctx->state[7] += h;
+}
