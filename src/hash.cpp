5b57824f86a201bb551c96752cde509033bb374b
spoturno <spoturno16@gmail.com>
2021-07-13 23:52:13 -0300

    sha256_final implemented

--- src/hash.cpp
+++ src/hash.cpp
@@ -15,6 +15,7 @@
 *************************************************/
 
 
+#include <cstring>
 #include <stdio.h>
 #include <stdlib.h>
 #include <memory.h>
@@ -115,5 +116,44 @@ void sha256_update(SHA256_CTX *ctx, const BYTE data[], size_t len){
             ctx->lenght = 0;
         }
     }
+}
+
+void sha256_final(SHA256_CTX *ctx, BYTE hash[]){
+   WORD i; 
+   i = ctx->lenght;
+   if(ctx->lenght < 56){
+       ctx->data[i++] = 0x80;
+       while (i < 56)
+           ctx->data[i++] = 0x00;
+   }else{
+       ctx->data[i++] = 0x80;
+       while(i < 64)
+           ctx->data[i++] = 0x00;
+       sha256_transform(ctx, ctx->data);
+       memset(ctx->data, 0, 56);
+   }
+    ctx->bitlenght += ctx->lenght * 8;
+    ctx->data[63] = ctx->bitlenght;
+    ctx->data[62] = ctx->bitlenght >> 8;
+    ctx->data[61] = ctx->bitlenght >> 16;
+    ctx->data[60] = ctx->bitlenght >> 24;
+    ctx->data[59] = ctx->bitlenght >> 32;
+    ctx->data[58] = ctx->bitlenght >> 40;
+    ctx->data[57] = ctx->bitlenght >> 48;
+    ctx->data[56] = ctx->bitlenght >> 56;
+    sha256_transform(ctx, ctx->data);
+   
+    // Since this implementation uses little endian byte ordering and SHA uses big endian,
+	// reverse all the bytes when copying the final state to the output hash.
+    for (i = 0; i < 4; ++i) {
+		hash[i]      = (ctx->state[0] >> (24 - i * 8)) & 0x000000ff;
+		hash[i + 4]  = (ctx->state[1] >> (24 - i * 8)) & 0x000000ff;
+		hash[i + 8]  = (ctx->state[2] >> (24 - i * 8)) & 0x000000ff;
+		hash[i + 12] = (ctx->state[3] >> (24 - i * 8)) & 0x000000ff;
+		hash[i + 16] = (ctx->state[4] >> (24 - i * 8)) & 0x000000ff;
+		hash[i + 20] = (ctx->state[5] >> (24 - i * 8)) & 0x000000ff;
+		hash[i + 24] = (ctx->state[6] >> (24 - i * 8)) & 0x000000ff;
+		hash[i + 28] = (ctx->state[7] >> (24 - i * 8)) & 0x000000ff;
+	}
 }
 
