217f6143205b585658f31e1d8c979531bae21c86
spoturno <spoturno16@gmail.com>
2021-07-28 20:46:27 -0300

    createEmptyTransactions, update createTransaction, addTran

--- src/transactions.cpp
+++ src/transactions.cpp
@@ -24,6 +24,16 @@ struct _rep_infotrac{
     struct tm * date;
 };
 
+bool isValidTransaction(Transaction t){
+    return false;
+}
+
+Transactions createEmptyTransactions(){
+   Transactions trans = new _rep_transactions; 
+   trans->total_transactions = 0;
+   trans->tran = NULL;
+   return trans;
+}
 
 Transaction createTransation(nat amount, User receiver){
     Transaction t = new _rep_transaction;
@@ -32,11 +42,14 @@ Transaction createTransation(nat amount, User receiver){
     info->receiver = receiver;
     info->date = getCurrentTime();
     t->info = info;
+    t->left = t->right = NULL;
     return t;
 }
 
-Transactions addTransaction(Transactions t){
-    return NULL;
+Transactions addTransaction(Transaction to_add, Transactions t){
+    t->total_transactions++;
+    t->tran = addTran(to_add, t->tran);
+    return NULL
 }
 
 Transactions removeTransaction(Transaction to_remove, Transactions t){
