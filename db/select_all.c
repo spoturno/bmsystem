8bdcf9f490ddf7076e7427066a73e852405f4304
spoturno <spoturno16@gmail.com>
2021-08-07 00:31:14 -0300

    sqlite select from users table done

---
+++ db/select_all.c
@@ -0,0 +1,47 @@
+#include <sqlite3.h>
+#include <stdio.h>
+
+int callback(void*, int, char**, char**);
+
+int main(void){
+    sqlite3 *db;
+    char *err_msg = 0;
+
+    int rc = sqlite3_open("test.db", &db);
+
+    if(rc != SQLITE_OK ){
+        fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
+        return 1;
+    }
+
+    char *sql = "SELECT * FROM Users";
+
+    // handles if there is more than 1 colum
+    rc = sqlite3_exec(db, sql, callback, 0, &err_msg);
+
+    if(rc != SQLITE_OK){
+        fprintf(stderr, "Failed to select data\n");
+        fprintf(stderr, "SQL error: %s\n", err_msg);
+        sqlite3_free(err_msg);
+        sqlite3_close(db);
+        return 1;
+    }
+
+    sqlite3_close(db);
+    return 0;
+}
+
+int callback(void * NotUsed, int argc, char **argv, char **azColName){
+    NotUsed = 0;
+    // argc:#colums argv:array of string representing column names
+    for(int i =0; i < argc; i++){
+        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+    }
+    printf("\n");
+    return 0;
+}
+
+
+
+
+
